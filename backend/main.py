7e586f34deccf91af1cdc3b6a157b9c8493e8626
gamertop13lol <36103611+gamertop13lol@users.noreply.github.com>
2025-07-02 12:24:50 +0100

    fuse

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,38 @@
 from fastapi import FastAPI
 import database
+import os
 
 app = FastAPI()
 
+#if not os.path.isfile("database.sqlite"):
+#    database.initdb()
+#    createvalues()
+
+#def createvalues():
+#    database.query("INSERT INTO ninja VALUES (1, 1, 'Alice', NULL, 0), (2, 0, 'Bob', NULL, 0), (3, 1, 'Charlie', NULL, 1), (4, 0, 'Mateus', 3, 0)")
+#    database.query("INSERT INTO task VALUES ('fios'), ('cham-cham'), ('copos')")
+
 @app.get("/registar_impostor")
-def matar_ninja(ninja: int):
-    return {"ninjas": ninja}
+def registar_impostor(ninja: int):
+    return database.query('UPDATE ninja SET impostor=1 WHERE id=?', (impostor,))
 
 @app.get("/matar_ninja")
 def matar_ninja(impostor: int, ninja: int):
-    return {"impostor": impostor, "ninja": ninja}
+    if database.query('SELECT impostor FROM ninja WHERE id=?', (impostor,)):
+        database.query('UPDATE ninja SET killed_by=? WHERE id=?', (impostor, ninja))
+        database.query('UPDATE ninja SET cooldown=1 WHERE id=?', (impostor,))
+        return {"impostor": impostor, "ninja": ninja}
+    else:
+        return {"erro": "não é impostor"}
 
 @app.get("/completar_task")
 def completar_task(task: str, ninja: int):
-    return {"task": task, "ninja": ninja}
+    if database.query('SELECT impostor FROM ninja WHERE id=?', (ninja,)):
+        database.query('UPDATE ninja SET cooldown=0 WHERE id=?', (ninja,))
+        return {"impostor": ninja}
+    else:
+        database.query('INSERT INTO completed_task (ninja_id, task) VALUES (?, ?)', (ninja, task))
+        return {"task": task, "ninja": ninja}
 
 @app.get("/emergency_meeting_start")
 def emergency_meeting_start(type: str, ninja: int):
