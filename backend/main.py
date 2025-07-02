131c03373f096462ef430b5f5ecba00e6ff68770
gamertop13lol <36103611+gamertop13lol@users.noreply.github.com>
2025-07-02 11:11:28 +0100

    created api methods

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/registar_impostor")
+def matar_ninja(nome: str):
+    return {"nome": nome} # id
+
+@app.get("/matar_ninja")
+def matar_ninja(impostor: int, ninja: int):
+    return {"impostor": impostor, "ninja": ninja}
+
+@app.get("/completar_task")
+def completar_task(task: str, ninja: int):
+    return {"task": task, "ninja": ninja}
+
+@app.get("/emergency_meeting_start")
+def emergency_meeting_start(type: str, ninja: int):
+    return {"type": type, "ninja": ninja}
+
+@app.get("emergency_meeting_end")
+def emergency_meeting_end():
+    return {"end": "ok"}
+
+@app.get("/log")
+def log(index: int):
+    return {"log": log[index:]}
+
+@app.get("/info")
+def info():
+    return {"ninjas": [{"id": 0, "nome": "Bob", "impostor": True, "cooldown": False, "killed_by": 1}]}
+
+def emergency_meeting_on():
+    return {"emergency_meeting_on": False}
