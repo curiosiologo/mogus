038a0a85db948db6228fbdf2317d9bd3769aaad0
João Lobo <joao@lobo.is>
2025-07-04 13:56:18 +0100

    ops: support ngrok

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,5 +6,10 @@ import './style.css'
 import router from "./router"
 
 // Use old fetch but with custom ngrok bypass warning header
+const oldFetch = window.fetch;
+window.fetch = async (url) => {
+    const response = await oldFetch(url, {headers: {'ngrok-skip-browser-warning': 'true'}});
+    return response;
+}
 
 createApp(App).use(router).mount('#app')
